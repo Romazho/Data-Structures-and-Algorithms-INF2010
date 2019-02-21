ac3452455242d22320713ac1c61ebb212186f502
Kevin <kevinscottsciliento@gmail.com>
2019-02-20 19:38:57 -0500

    KC: Ajout de la Partie 1 qui compile. Par exemple je ne pense pas qu'on ai du O(n)

--- TP3/partie1/projet1/src/projet1/BinaryNode.java
+++ TP3/partie1/projet1/src/projet1/BinaryNode.java
@@ -24,13 +24,20 @@ public class BinaryNode<T extends Comparable<? super T>> {
 	// TODO: on ajoute une nouvelle donnee au bon endroit
 	// O(log(n))
 	public void insert(T item) {
-		
+
 		if (data.compareTo(item) < 0) {
-			right = new BinaryNode<T>(item);
+			if (right == null)
+				right = new BinaryNode<T>(item);
+			else
+				right.insert(item);
+
 		} else {
-			left = new BinaryNode<T>(item);
+			if (left == null)
+				left = new BinaryNode<T>(item);
+			else
+				left.insert(item);
 		}
-		
+
 	}
 
 	// TODO: est-ce que l'item fais partie du noeuds courant
@@ -40,16 +47,14 @@ public class BinaryNode<T extends Comparable<? super T>> {
 		if (item.compareTo(data) == 0) {
 			return true;
 
-		}
-		else if ((right == null) && (data.compareTo(item) < 0)) {
+		} else if ((right != null) && (data.compareTo(item) < 0)) {
 			return right.contains(item);
 		}
 
-		else if ((left == null) && (data.compareTo(item) >= 0)) {
+		else if ((left != null) && (data.compareTo(item) >= 0)) {
 			return left.contains(item);
 
-		} 
-		else {
+		} else {
 
 			return false;
 		}
@@ -58,48 +63,38 @@ public class BinaryNode<T extends Comparable<? super T>> {
 	// TODO: on retourne la maximale de l'arbre
 	// O(n)
 	public int getHeight() {
+
+		int hauteurGauche = -1, hauteurDroit = -1; // On ne peut pas les d�clarer dans les if
 		
-		if(this == null) {
+		if ((left == null) && (right == null)) //Si le dernier noeud
 			return -1;
-		}
-		else {
-			return 1 + Math.max(left.getHeight(),right.getHeight());
-		}
 		
+		if (left != null) 
+			hauteurGauche = left.getHeight();
+		
+		if (right != null)
+			hauteurDroit = right.getHeight();
+
+		if (hauteurGauche > hauteurDroit) // On veux seulement la valeur la plus grosse
+			return (++hauteurGauche);
+		else
+			return (++hauteurDroit);
 	}
 
 	// TODO: l'ordre d'insertion dans la liste est l'ordre logique
 	// de manière que le plus petit item sera le premier inseré
 	// O(n)
 	public void fillListInOrder(List<BinaryNode<T>> result) {
-		
-		left.fillListInOrder(result); //on va a gauche
-		
-		if(left == null) {
+		if (left == null) { //Verifier que l'element suivant n'est pas null avant
 			return;
 		}
-		
-		result.add(this);				//on ajoute l'element
-		
-		right.fillListInOrder(result);	//on va a droite
-		
-		if(right == null) {
+		left.fillListInOrder(result); // on va a gauche
+
+		result.add(this); // on ajoute l'element
+
+		if (right == null) { //Verifier que l'element suivant n'est pas null avant
 			return;
 		}
-		
-		
+		right.fillListInOrder(result); // on va a droite
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TP3/partie1/projet1/src/projet1/Main.java
+++ TP3/partie1/projet1/src/projet1/Main.java
@@ -9,6 +9,19 @@ public class Main {
         System.out.println("Debut des tests du TP3");
         TestBinary testBinary = new TestBinary();
         testBinary.test();
-
+        testBinary.test();
+        /*DEBUG
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>(5);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(4);
+        
+       if( tree.contains(3))
+    	   System.out.println("Contient 3");
+       System.out.println(tree.getHeight());
+       */
     }
+    
 }
